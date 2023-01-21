3be4e43bc7a12f1621a89803e7621c67b3bff2e6
covik <6125218+Covik@users.noreply.github.com>
2023-01-22 00:54:58 +0100

    feat(app): use FiraGO font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/firago/300.css';
+import '@fontsource/firago/400.css';
+import '@fontsource/firago/500.css';
+import '@fontsource/firago/700.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
