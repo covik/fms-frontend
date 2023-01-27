9273ae1c89f17beeb0a66a3835cbd654934e9533
covik <6125218+Covik@users.noreply.github.com>
2023-01-27 10:04:24 +0100

    chore(app): scaffold basic layout with navigation drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0);
+import {
+  AccountTieHat,
+  Bell,
+  Cog,
+  HammerWrench,
+  Power,
+  Truck,
+  TruckTrailer,
+} from 'mdi-material-ui';
 
+const navigationItems = [
+  {
+    id: 'vehicles',
+    title: 'Vozila',
+    icon: <Truck />,
+  },
+  {
+    id: 'trailers',
+    title: 'Prikolice',
+    icon: <TruckTrailer />,
+  },
+  {
+    id: 'drivers',
+    title: 'Vozači',
+    icon: <AccountTieHat />,
+  },
+  {
+    id: 'servicing',
+    title: 'Servisi',
+    icon: <HammerWrench />,
+  },
+  {
+    id: 'notifications',
+    title: 'Obavijesti',
+    icon: <Bell />,
+  },
+  {
+    id: 'settings',
+    title: 'Postavke',
+    icon: <Cog />,
+  },
+  {
+    id: 'logout',
+    title: 'Odjava',
+    icon: <Power />,
+  },
+];
+
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Drawer anchor="left" variant="permanent">
+        <List disablePadding>
+          {navigationItems.map(({ id, title, icon }) => (
+            <ListItem key={id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText primary={title} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { CssBaseline, createTheme, ThemeProvider } from '@mui/material';
+import { App } from './App';
 import '@fontsource/firago/300.css';
 import '@fontsource/firago/400.css';
 import '@fontsource/firago/500.css';
 import '@fontsource/firago/700.css';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'FiraGO, sans-serif',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
