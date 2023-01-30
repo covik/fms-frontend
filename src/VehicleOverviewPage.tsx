b3c05e7db37cc049e8a507a990dd2d71f8494640
covik <6125218+Covik@users.noreply.github.com>
2023-01-30 20:11:16 +0100

    feat(app): design vehicle overview header

--- src/VehicleOverviewPage.tsx
+++ src/VehicleOverviewPage.tsx
@@ -1,5 +1,17 @@
 import { VehicleAttributes, VehicleCard } from './VehicleCard';
-import { Box, useTheme } from '@mui/material';
+import {
+  Box,
+  FormControlLabel,
+  IconButton,
+  Menu,
+  MenuItem,
+  Switch,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import { Cog } from 'mdi-material-ui';
+import { useState } from 'react';
+import type { MouseEvent } from 'react';
 
 const vehicles: (VehicleAttributes & { id: string })[] = [
   {
@@ -10,19 +22,70 @@ const vehicles: (VehicleAttributes & { id: string })[] = [
   },
 ];
 
+function PageTitle() {
+  return (
+    <Typography component="h1" variant="h3" color="grey" fontWeight="medium">
+      Vozila
+    </Typography>
+  );
+}
+
+function Settings() {
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(anchor);
+  const openMenu = (e: MouseEvent<HTMLButtonElement>) =>
+    void setAnchor(e.currentTarget);
+  const closeMenu = () => void setAnchor(null);
+
+  return (
+    <>
+      <IconButton size="medium" onClick={openMenu}>
+        <Cog fontSize="medium" />
+      </IconButton>
+      <Menu id="basic-menu" anchorEl={anchor} open={isOpen} onClose={closeMenu}>
+        <MenuItem disableRipple>
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label="Prikaži kartu"
+          />
+        </MenuItem>
+        <MenuItem disableRipple>
+          <FormControlLabel control={<Switch />} label="Satelitske karte" />
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
+
+function PageHeader() {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <PageTitle />
+      </Box>
+      <Box sx={{ flexGrow: 0 }}>
+        <Settings />
+      </Box>
+    </Box>
+  );
+}
+
 export function VehicleOverviewPage() {
   const theme = useTheme();
 
   return (
-    <Box sx={{ padding: theme.spacing(1) }}>
-      {vehicles.map((vehicle) => (
-        <VehicleCard
-          key={vehicle.id}
-          name={vehicle.name}
-          movement={vehicle.movement}
-          ignition={vehicle.ignition}
-        />
-      ))}
+    <Box sx={{ padding: theme.spacing(2) }}>
+      <PageHeader />
+      <Box sx={{ marginTop: theme.spacing(2) }}>
+        {vehicles.map((vehicle) => (
+          <VehicleCard
+            key={vehicle.id}
+            name={vehicle.name}
+            movement={vehicle.movement}
+            ignition={vehicle.ignition}
+          />
+        ))}
+      </Box>
     </Box>
   );
 }
