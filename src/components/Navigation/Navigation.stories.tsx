5f378c654cd6cb9fcc8a48ca6b681f6858c0b8bd
covik <6125218+Covik@users.noreply.github.com>
2023-02-01 14:20:09 +0100

    test: add Navigation rail story

--- src/components/Navigation/Navigation.stories.tsx
+++ src/components/Navigation/Navigation.stories.tsx
@@ -1,11 +1,19 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Navigation } from './Navigation';
+import { styled } from '@mui/material';
 
 export default {
   title: 'Navigation',
   component: Navigation,
+  parameters: {
+    layout: 'centered',
+  },
 } as ComponentMeta<typeof Navigation>;
 
-const Template: ComponentStory<typeof Navigation> = () => <Navigation />;
+const StyledNavigation = styled(Navigation)({
+  position: 'static',
+});
+
+const Template: ComponentStory<typeof Navigation> = () => <StyledNavigation />;
 
 export const Rail = Template.bind({});
