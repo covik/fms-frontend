80d415ec78ce1884a0f05b927d8bc197aebcd059
covik <6125218+Covik@users.noreply.github.com>
2023-02-01 11:22:49 +0100

    fix(app): redesign VehicleCard to appear as a list item

--- src/VehicleCard.tsx
+++ src/VehicleCard.tsx
@@ -1,31 +1,6 @@
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-} from '@mui/material';
-import {
-  CarBattery,
-  DotsVertical,
-  History,
-  Pencil,
-  ShareVariant,
-  Speedometer,
-  TruckFast,
-  Wrench,
-} from 'mdi-material-ui';
+import { Card, CardHeader, IconButton } from '@mui/material';
+import { ShareVariant } from 'mdi-material-ui';
 import fakeGoogleMap from './assets/google-map-fixtures/moving.png';
-import { useState } from 'react';
-import type { MouseEvent } from 'react';
 
 export interface VehicleAttributes {
   name: string;
@@ -36,16 +11,10 @@ export interface VehicleAttributes {
 export function VehicleCard({ name, ignition, movement }: VehicleAttributes) {
   const headerColor = 'green';
 
-  const [anchor, setAnchor] = useState<undefined | HTMLElement>(undefined);
-  const open = Boolean(anchor);
-  const openVehicleActions = (e: MouseEvent<HTMLButtonElement>) =>
-    setAnchor(e.currentTarget);
-  const closeVehicleActions = () => setAnchor(undefined);
-
   return (
-    <Card sx={{ maxWidth: '360px' }}>
+    <Card>
       <CardHeader
-        avatar={<TruckFast fontSize="large" htmlColor={headerColor} />}
+        avatar={<img src={fakeGoogleMap} />}
         title={name}
         titleTypographyProps={{
           color: headerColor,
@@ -53,66 +22,13 @@ export function VehicleCard({ name, ignition, movement }: VehicleAttributes) {
           component: 'h1',
         }}
         subheader="prije 2 minute"
+        action={
+          <IconButton sx={{ marginTop: 1, marginRight: 1 }}>
+            <ShareVariant fontSize="small" />
+          </IconButton>
+        }
+        sx={{ padding: 1 }}
       />
-      <CardMedia component="img" image={fakeGoogleMap} />
-      <CardContent sx={{ display: 'none' }}>
-        <List disablePadding dense>
-          <ListItem disableGutters>
-            <ListItemIcon sx={{ minWidth: '40px' }}>
-              <Speedometer />
-            </ListItemIcon>
-            <ListItemText primary="40 km/h" />
-          </ListItem>
-          <ListItem disableGutters>
-            <ListItemIcon sx={{ minWidth: '40px' }}>
-              <CarBattery />
-            </ListItemIcon>
-            <ListItemText primary="12.90 V" />
-          </ListItem>
-        </List>
-      </CardContent>
-      <CardActions sx={{ justifyContent: 'flex-end' }}>
-        <IconButton>
-          <ShareVariant />
-        </IconButton>
-        <IconButton onClick={openVehicleActions}>
-          <DotsVertical />
-        </IconButton>
-        <Menu
-          open={open}
-          onClose={closeVehicleActions}
-          anchorEl={anchor}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          transformOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
-          elevation={4}
-        >
-          <MenuItem dense>
-            <ListItemIcon>
-              <Pencil />
-            </ListItemIcon>
-            <ListItemText>Preimenuj</ListItemText>
-          </MenuItem>
-          <Divider />
-          <MenuItem dense>
-            <ListItemIcon>
-              <History />
-            </ListItemIcon>
-            <ListItemText>Lokacije</ListItemText>
-          </MenuItem>
-          <MenuItem dense>
-            <ListItemIcon>
-              <Wrench />
-            </ListItemIcon>
-            <ListItemText primary="Servisi"></ListItemText>
-          </MenuItem>
-        </Menu>
-      </CardActions>
     </Card>
   );
 }
