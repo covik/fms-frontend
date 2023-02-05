06f15216b32fe4582693de1e050231461c713848
covik <6125218+Covik@users.noreply.github.com>
2023-02-05 11:30:19 +0100

    test: refactor Map stories to be more flexible about map centering

--- src/components/Map/Map.stories.tsx
+++ src/components/Map/Map.stories.tsx
@@ -1,18 +1,23 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Map } from './Map';
 
-export default {
-  title: 'Map',
-  component: Map,
-} as ComponentMeta<typeof Map>;
-
 const croatia = {
   x: 44.698832,
   y: 16.373162,
 };
 
+export default {
+  title: 'Map',
+  component: Map,
+  args: {
+    x: croatia.x,
+    y: croatia.y,
+    z: 6,
+  },
+} as ComponentMeta<typeof Map>;
+
 const Template: ComponentStory<typeof Map> = ({ x, y, z, ...args }) => (
-  <Map x={croatia.x} y={croatia.y} z={6} {...args} />
+  <Map x={x} y={y} z={z} {...args} />
 );
 
 export const Basic = Template.bind({});
