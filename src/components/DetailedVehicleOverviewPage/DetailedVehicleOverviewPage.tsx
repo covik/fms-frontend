142e919e99bdd7cf9948392b01c1af9c679aff8b
covik <6125218+Covik@users.noreply.github.com>
2023-02-13 17:45:50 +0100

    refactor(app): extract live preview code to a custom component

--- src/components/DetailedVehicleOverviewPage/DetailedVehicleOverviewPage.tsx
+++ src/components/DetailedVehicleOverviewPage/DetailedVehicleOverviewPage.tsx
@@ -1,7 +1,6 @@
 import { Box, Card, Tab, Tabs, Typography } from '@mui/material';
-import { Map } from '../Map';
-import { LatestInformation } from './LatestInformation';
 import { RouteBrowser } from './RouteBrowser';
+import { LivePreview } from './LivePreview';
 
 export interface DetailedVehicleOverviewPageAttributes {
   activeTab: 'live-updates' | 'routes' | 'maintenance';
@@ -13,21 +12,7 @@ export function DetailedVehicleOverviewPage({
   const tabs = [
     {
       id: 'live-updates',
-      children: (
-        <Box>
-          <Card sx={{ height: '40vh', minHeight: '200px', marginBottom: 1.5 }}>
-            <Map
-              x={44.698832}
-              y={16.373162}
-              z={6}
-              width="100%"
-              height="100%"
-              noControls
-            />
-          </Card>
-          <LatestInformation />
-        </Box>
-      ),
+      children: <LivePreview />,
     },
     {
       id: 'routes',
