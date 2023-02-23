d65ce7242fc10affb397018b1c8f1419848fd08d
covik <6125218+Covik@users.noreply.github.com>
2023-02-23 11:34:49 +0100

    fix(http-client): make second parameter optional

--- src/lib/HttpClient/HttpClient.ts
+++ src/lib/HttpClient/HttpClient.ts
@@ -2,7 +2,7 @@ import { HttpClientException } from './Exceptions';
 
 export async function request(
   input: string,
-  init: RequestInit,
+  init?: RequestInit,
 ): Promise<Response> {
   return new Promise((resolve, reject) => {
     reject(new HttpClientException(new DOMException('AbortError')));
