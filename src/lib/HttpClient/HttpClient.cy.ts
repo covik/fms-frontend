e792cc40d34d7982772b4bcf54a302fee2abd4af
covik <6125218+Covik@users.noreply.github.com>
2023-03-23 15:00:46 +0100

    refactor(http-client): rename tests and refactor some concepts

--- src/lib/HttpClient/HttpClient.cy.ts
+++ src/lib/HttpClient/HttpClient.cy.ts
@@ -5,63 +5,61 @@ import {
   HttpServerException,
 } from './';
 
-describe('Client errors', () => {
-  const clientSideProblems = [
+describe('Request error', () => {
+  const requestErrors = [
     {
-      testName: `should throw ${HttpClientException.name} given an invalid URL`,
-      requestFunction: () => request('https://'),
-      errorName: 'TypeError',
-      message:
+      description: `given an invalid URL then throw ${HttpClientException.name}`,
+      arrange: () => request('https://'),
+      expectedError: 'TypeError',
+      expectedMessage:
         "Failed to construct 'Request': Failed to parse URL from https://",
     },
     {
-      testName: `should throw ${HttpClientException.name} given a username and password are part of URL`,
-      requestFunction: () => request('http://user:password@example.com/'),
-      errorName: 'TypeError',
-      message:
+      description: `given URL contains username and password then throw ${HttpClientException.name}`,
+      arrange: () => request('http://user:password@example.com/'),
+      expectedError: 'TypeError',
+      expectedMessage:
         "Failed to construct 'Request': Request cannot be constructed from a URL that includes credentials: http://user:password@example.com/",
     },
     {
-      testName: `should throw ${HttpClientException.name} given an invalid header name`,
-      requestFunction: () =>
+      description: `given an invalid header name then throw ${HttpClientException.name}`,
+      arrange: () =>
         request('http://localhost', {
           headers: { 'C ontent-Type': 'text/xml' },
         }),
-      errorName: 'TypeError',
-      message: "Failed to construct 'Request': Invalid name",
+      expectedError: 'TypeError',
+      expectedMessage: "Failed to construct 'Request': Invalid name",
     },
     {
-      testName: `should throw ${HttpClientException.name} if request gets aborted`,
-      requestFunction: async () => {
+      description: `given a valid request when it gets aborted then throw ${HttpClientException.name} if `,
+      arrange: async () => {
         const controller = new AbortController();
         const signal = controller.signal;
         const r = request('http://localhost', { signal });
         controller.abort();
         await r;
       },
-      errorName: 'AbortError',
-      message: 'This operation was aborted',
+      expectedError: 'AbortError',
+      expectedMessage: 'This operation was aborted',
     },
   ];
 
-  clientSideProblems.forEach(
-    ({ testName, requestFunction, errorName, message }) => {
-      it(testName, () => {
-        cy.testException(requestFunction).then((theError) => {
-          theError().should('be.instanceOf', HttpClientException);
-          theError().should('have.property', 'originalError');
+  requestErrors.forEach((situation) => {
+    specify(situation.description, () => {
+      cy.testException(situation.arrange).then((outcome) => {
+        outcome().should('be.instanceOf', HttpClientException);
+        outcome().should('have.property', 'originalError');
 
-          theError()
-            .its('originalError')
-            .should('have.property', 'name', errorName);
+        outcome()
+          .its('originalError')
+          .should('have.property', 'name', situation.expectedError);
 
-          theError()
-            .its('originalError')
-            .should('have.property', 'message', message);
-        });
+        outcome()
+          .its('originalError')
+          .should('have.property', 'message', situation.expectedMessage);
       });
-    },
-  );
+    });
+  });
 });
 
 describe('Network error', () => {
@@ -73,17 +71,18 @@ describe('Network error', () => {
   });
 });
 
-describe('Server errors', () => {
+describe('Response error', () => {
   describe(`should throw ${HttpServerException.name} if a response status code is not 200-299`, () => {
     it('1xx: not testable', () => {
       cy.log('the tests hang until timeout');
     });
     it('2xx: no exception', () => {
-      let code = -1;
-      cy.intercept('http://localhost', (res) => res.reply(code));
-
-      cy.then(() => (code = 200)).then(assertServerResponse().NotError);
-      cy.then(() => (code = 299)).then(assertServerResponse().NotError);
+      cy.intercept('http://localhost', { statusCode: 200 }).then(
+        assertServerResponse().NotError,
+      );
+      cy.intercept('http://localhost', { statusCode: 299 }).then(
+        assertServerResponse().NotError,
+      );
     });
     it('3xx: not testable', () => {
       cy.log('after issuing 3xx code every request afterwards resolves to 3xx');
@@ -100,7 +99,7 @@ describe('Server errors', () => {
   });
 });
 
-describe('Successful response', () => {
+describe('Response succeeded', () => {
   it('should return Response object with correct body and code', () => {
     const url = 'http://localhost';
     const statusCode = 200;
