d86fda1c0aeb5a43649035e331d75bba097e6e7f
covik <6125218+Covik@users.noreply.github.com>
2023-04-03 11:04:12 +0200

    feat(app): add AuthenticatedApp component

---
+++ src/foundation/AuthenticatedApp/AuthenticatedApp.tsx
@@ -0,0 +1,9 @@
+import { BottomNavigationLayout } from '../../components/AppShell';
+
+export function AuthenticatedApp() {
+  return (
+    <BottomNavigationLayout>
+      <div>To be continued...</div>
+    </BottomNavigationLayout>
+  );
+}
--- src/foundation/index.ts
+++ src/foundation/index.ts
@@ -1,3 +1,4 @@
 export { App } from './App';
+export { AuthenticatedApp } from './AuthenticatedApp';
 export { DesignBaseline } from './DesignBaseline';
 export { AuthProvider, useAuth } from './context/Auth';
