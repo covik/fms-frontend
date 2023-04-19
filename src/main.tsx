ac4d8500b24b47c3f1027f6ed5a728fd7f9eea57
covik <6125218+Covik@users.noreply.github.com>
2023-04-19 19:18:51 +0200

    chore(app): initialize React Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
 import {
   App,
   AuthProvider,
@@ -7,12 +9,16 @@ import {
   AuthenticatedApp,
 } from './foundation';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <DesignBaseline>
-    <AuthProvider>
-      <App>
-        <AuthenticatedApp />
-      </App>
-    </AuthProvider>
-  </DesignBaseline>,
+  <QueryClientProvider client={queryClient}>
+    <DesignBaseline>
+      <AuthProvider>
+        <App>
+          <AuthenticatedApp />
+        </App>
+      </AuthProvider>
+    </DesignBaseline>
+  </QueryClientProvider>,
 );
