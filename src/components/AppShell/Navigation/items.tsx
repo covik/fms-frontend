d72e61af35993726fe14fff2d6a1e396011c15a2
covik <6125218+Covik@users.noreply.github.com>
2023-04-20 19:38:48 +0200

    feat(app): add map item to navigation

--- src/components/AppShell/Navigation/items.tsx
+++ src/components/AppShell/Navigation/items.tsx
@@ -1,6 +1,11 @@
-import { Bell, Truck } from 'mdi-material-ui';
+import { Bell, Map, Truck } from 'mdi-material-ui';
 
 export const all = [
+  {
+    id: 'map',
+    title: 'Karta',
+    icon: <Map />,
+  },
   {
     id: 'vehicles',
     title: 'Vozila',
