eb2563cd00e98a64e467bff99b977fd770da7379
covik <6125218+Covik@users.noreply.github.com>
2023-05-06 10:18:31 +0200

    feat(app): implement minimal PWA requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pwa()],
   server: {
     proxy: {
       '/api': {
@@ -13,3 +14,35 @@ export default defineConfig({
     },
   },
 });
+
+function pwa() {
+  return VitePWA({
+    manifest: {
+      name: 'Zara Fleet',
+      short_name: 'Zara Fleet',
+      description: 'Application for managing fleet of vehicles',
+      display: 'standalone',
+      orientation: 'portrait',
+      background_color: '#edeff0',
+      theme_color: '#edeff0',
+      icons: [
+        {
+          src: 'icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/maskable-icon.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+  });
+}
