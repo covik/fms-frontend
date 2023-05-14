8a1856c1eeb0ddbeebf2e7f6634f6180eb565006
covik <6125218+Covik@users.noreply.github.com>
2023-05-14 19:16:07 +0200

    feat(vehicle-service): add VehicleNotFound exception

--- src/lib/VehicleService/index.ts
+++ src/lib/VehicleService/index.ts
@@ -1,8 +1,10 @@
 import { fetchAll } from './VehicleService';
 import { takeOnlyOperational, takeOnlyTimedOut } from './Filter';
+import { NotFoundException } from './Exception';
 
 export const Vehicle = {
   fetchAll,
   takeOnlyOperational,
   takeOnlyTimedOut,
+  NotFoundException,
 };
