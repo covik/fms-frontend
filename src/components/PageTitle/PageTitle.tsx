a46f39003cabf6db378b81f714e4cea9ef388211
covik <6125218+Covik@users.noreply.github.com>
2023-05-19 00:08:38 +0200

    feat(app): add spacing bellow page title

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -2,7 +2,12 @@ import { Typography } from '@mui/material';
 
 export function PageTitle({ children }: { children: string }) {
   return (
-    <Typography component="h1" variant="h3" fontWeight="medium">
+    <Typography
+      component="h1"
+      variant="h3"
+      fontWeight="medium"
+      marginBottom={1}
+    >
       {children}
     </Typography>
   );
