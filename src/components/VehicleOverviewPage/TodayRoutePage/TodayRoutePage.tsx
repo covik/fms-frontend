ab0f0a5cea5504d7afcaeedf4389f32b067e3722
covik <6125218+Covik@users.noreply.github.com>
2023-05-22 15:25:20 +0200

    feat(app): render vehicle stops as table in today route page

--- src/components/VehicleOverviewPage/TodayRoutePage/TodayRoutePage.tsx
+++ src/components/VehicleOverviewPage/TodayRoutePage/TodayRoutePage.tsx
@@ -12,13 +12,14 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Skeleton,
   Stack,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
-  useTheme,
+  Typography,
 } from '@mui/material';
 import { Map } from '../../Map';
 import {
@@ -37,6 +38,8 @@ import { endOfDay, startOfDay } from 'date-fns';
 import { useParams } from '@tanstack/router';
 import { Vehicle } from '../../../lib/VehicleService';
 import { VehicleRoute, VehicleRouteStops } from '../../VehicleRoute';
+import { RouteStop } from '../../../models/RouteStop';
+import { useDateTime } from '../../../foundation';
 
 const routeColor = '#BA68C8';
 const today = new Date();
@@ -96,7 +99,7 @@ export function TodayRoutePage() {
             </Tile>
             <Tile label={'Zaustavljanje'}>
               <CardContent>
-                <StopsTable />
+                <StopsTable stops={stopsQuery.data} />
               </CardContent>
             </Tile>
           </Stack>
@@ -232,8 +235,16 @@ function Summary() {
   );
 }
 
-function StopsTable() {
-  const theme = useTheme();
+function StopsTable({ stops }: { stops: RouteStop[] | undefined }) {
+  const { formatTime, formatDuration } = useDateTime();
+
+  if (stops && stops.length === 0) {
+    return <Typography variant={'body2'}>Nema zaustavljanja</Typography>;
+  }
+
+  const indexWidth = '30px';
+  const departureWidth = '60px';
+  const arrivalWidth = '60px';
 
   return (
     <Table
@@ -246,57 +257,64 @@ function StopsTable() {
     >
       <TableHead>
         <TableRow
-          sx={{
+          sx={(theme) => ({
             '& .MuiTableCell-root': {
               paddingBottom: '0 !important',
               textTransform: 'uppercase',
               color: `${theme.palette.text.secondary} !important`,
             },
-          }}
+          })}
         >
-          <TableCell sx={{ width: '30px' }}>#</TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
+          <TableCell sx={{ width: indexWidth }}>#</TableCell>
+          <TableCell align={'center'} sx={{ width: departureWidth }}>
             Od
           </TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
+          <TableCell align={'center'} sx={{ width: arrivalWidth }}>
             Do
           </TableCell>
           <TableCell>Trajanje</TableCell>
         </TableRow>
       </TableHead>
-      <TableBody sx={{ '& .MuiTableCell-root': { color: 'text.primary' } }}>
-        <TableRow>
-          <TableCell sx={{ width: '30px' }}>1</TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            01:10
-          </TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            08:10
-          </TableCell>
-          <TableCell>7h</TableCell>
-        </TableRow>
 
-        <TableRow>
-          <TableCell sx={{ width: '30px' }}>2</TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            13:00
-          </TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            15:12
-          </TableCell>
-          <TableCell>2h 12m</TableCell>
-        </TableRow>
-
-        <TableRow sx={{ '&:last-of-type td': { border: 0 } }}>
-          <TableCell sx={{ width: '30px' }}>3</TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            16:20
-          </TableCell>
-          <TableCell align={'center'} sx={{ width: '60px' }}>
-            18:11
-          </TableCell>
-          <TableCell>1h 51m</TableCell>
-        </TableRow>
+      <TableBody
+        sx={{
+          '& .MuiTableCell-root': { color: 'text.primary' },
+          '& > tr:last-of-type td': { border: 0 },
+        }}
+      >
+        {stops !== undefined ? (
+          stops.map((stop, index) => (
+            <TableRow key={stop.id()}>
+              <TableCell sx={{ width: indexWidth }}>{index + 1}</TableCell>
+              <TableCell align={'center'} sx={{ width: departureWidth }}>
+                {formatTime(stop.startTime())}
+              </TableCell>
+              <TableCell align={'center'} sx={{ width: arrivalWidth }}>
+                {formatTime(stop.endTime())}
+              </TableCell>
+              <TableCell>{formatDuration(stop.duration())}</TableCell>
+            </TableRow>
+          ))
+        ) : (
+          <>
+            {[1, 2, 3].map((index) => (
+              <TableRow key={index}>
+                <TableCell sx={{ width: indexWidth }}>
+                  <Skeleton variant={'text'} />
+                </TableCell>
+                <TableCell align={'center'} sx={{ width: departureWidth }}>
+                  <Skeleton variant={'text'} />
+                </TableCell>
+                <TableCell align={'center'} sx={{ width: arrivalWidth }}>
+                  <Skeleton variant={'text'} />
+                </TableCell>
+                <TableCell>
+                  <Skeleton variant={'text'} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </>
+        )}
       </TableBody>
     </Table>
   );
