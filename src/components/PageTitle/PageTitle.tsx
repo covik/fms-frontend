e84e8ceb7080b7d1076856b8bbe812b48a8f7cdc
covik <6125218+Covik@users.noreply.github.com>
2023-06-05 17:30:05 +0200

    feat(app): add page title skeleton

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Skeleton, Typography, useTheme } from '@mui/material';
 
 export function PageTitle({ children }: { children: string }) {
   return (
@@ -10,5 +10,16 @@ export function PageTitle({ children }: { children: string }) {
     >
       {children}
     </Typography>
+  );
+}
+
+export function PageTitleSkeleton() {
+  const theme = useTheme();
+  return (
+    <Skeleton
+      variant={'text'}
+      width={'150px'}
+      height={theme.typography.h3.fontSize}
+    />
   );
 }
--- src/components/PageTitle/index.ts
+++ src/components/PageTitle/index.ts
@@ -1,1 +1,1 @@
-export { PageTitle } from './PageTitle';
+export { PageTitle, PageTitleSkeleton } from './PageTitle';
