b4ff36b6874478d9f4816e9c1ca2e9a48419af40
covik <6125218+Covik@users.noreply.github.com>
2023-06-08 11:33:31 +0200

    refactor(measurement-unit): remove useless describe() block from Speed.cy.ts

--- src/lib/MeasurementUnit/Speed/Speed.cy.ts
+++ src/lib/MeasurementUnit/Speed/Speed.cy.ts
@@ -1,9 +1,7 @@
 import { Knots, KPH, MPH } from '.';
 
-describe('Speed', () => {
-  specify('validate unit symbols', () => {
-    expect(new KPH(0).symbol()).to.equal('km/h');
-    expect(new MPH(0).symbol()).to.equal('mph');
-    expect(new Knots(0).symbol()).to.equal('kt');
-  });
+specify('validate unit symbols', () => {
+  expect(new KPH(0).symbol()).to.equal('km/h');
+  expect(new MPH(0).symbol()).to.equal('mph');
+  expect(new Knots(0).symbol()).to.equal('kt');
 });
