ebe89181b6c6d9899bcc1a09c944ca71fdb180c4
covik <6125218+Covik@users.noreply.github.com>
2023-06-10 14:39:11 +0200

    test(app): add no updates story for UpdateReadyBanner

--- src/components/UpdateReadyBanner/UpdateReadyBanner.stories.tsx
+++ src/components/UpdateReadyBanner/UpdateReadyBanner.stories.tsx
@@ -2,13 +2,26 @@ import type { Meta } from '@storybook/react';
 import { VersionProvider } from '../../foundation';
 import { UpdateReadyBanner } from './UpdateReadyBanner';
 
+const mockApplyUpdate = () => {
+  console.log('apply update clicked (mock)');
+};
+
 export default {
   component: UpdateReadyBanner,
+} satisfies Meta<typeof UpdateReadyBanner>;
+
+export const UpdateReady = {
   render: () => (
-    <VersionProvider isUpdateReady={true} applyUpdate={() => {}}>
+    <VersionProvider isUpdateReady={true} applyUpdate={mockApplyUpdate}>
       <UpdateReadyBanner />
     </VersionProvider>
   ),
-} satisfies Meta<typeof UpdateReadyBanner>;
+};
 
-export const Default = {};
+export const NoUpdate = {
+  render: () => (
+    <VersionProvider isUpdateReady={false} applyUpdate={mockApplyUpdate}>
+      <UpdateReadyBanner />
+    </VersionProvider>
+  ),
+};
