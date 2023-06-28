65626e04f9070a907eed7b1ed30710f2244b006d
covik <6125218+Covik@users.noreply.github.com>
2023-06-28 18:44:42 +0200

    fix(ui-molecule): allow RouterNavigationProvider to be instantiated only once in the app

--- src/ui/molecules/navigation/router-navigation-provider.tsx
+++ src/ui/molecules/navigation/router-navigation-provider.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react';
-import { useMatches } from '@tanstack/router';
+import { useRouter } from '@tanstack/router';
 import { NavigationProvider } from './navigation-provider';
 import { RouterTabRender } from './navigation-item-renderer';
 import type {
@@ -13,7 +13,10 @@ export interface RouterNavigationProviderAttributes
 export function RouterNavigationProvider({
   children,
 }: RouterNavigationProviderAttributes) {
-  const matches = useMatches();
+  const router = useRouter();
+  const currentLocation = router.state.currentLocation;
+  const matches = router.matchRoutes(currentLocation.pathname);
+
   const resolveActiveItemFromMatches =
     useCallback<ActiveNavigationItemResolver>(
       (items) => {
@@ -22,8 +25,9 @@ export function RouterNavigationProvider({
         const fullPaths: string[] = routesWithoutParentRoute.map(
           (route) => route.fullPath,
         );
+        const itemsShallowCopy = [...items];
 
-        return items.find((item) => {
+        return itemsShallowCopy.reverse().find((item) => {
           if (fullPaths.includes(item.to ?? '')) return item;
         });
       },
