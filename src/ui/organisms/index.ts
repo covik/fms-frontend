8a7e6555483d9a3fc57c8fc2b418d35a386829e6
covik <6125218+Covik@users.noreply.github.com>
2023-06-29 19:05:10 +0200

    feat(ui-organism): add vehicle-loading-indicator

--- src/ui/organisms/index.ts
+++ src/ui/organisms/index.ts
@@ -2,4 +2,5 @@ export * from './google-maps';
 export * from './location-state-block';
 export * from './network-state-block';
 export * from './position-info-window';
+export * from './vehicle-loading-indicator';
 export * from './vehicle-state-block';
