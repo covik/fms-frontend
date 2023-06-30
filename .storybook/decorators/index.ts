06e7aee7b19ae0276cc9408fb95dd5fcbd2291f4
covik <6125218+Covik@users.noreply.github.com>
2023-06-30 10:23:34 +0200

    test: add storybook version decorator

--- .storybook/decorators/index.ts
+++ .storybook/decorators/index.ts
@@ -1,1 +1,2 @@
 export * from './navigation-decorator';
+export * from './version-decorator';
---
+++ .storybook/decorators/version-decorator.tsx
@@ -0,0 +1,14 @@
+import { VersionProvider } from '../../src/foundation';
+import type { Decorator } from '@storybook/react';
+
+const mockApplyUpdate = () => {
+  console.log('apply update clicked (mock)');
+};
+
+export function withAppUpdate(updateReady: boolean): Decorator {
+  return (Story) => (
+    <VersionProvider isUpdateReady={updateReady} applyUpdate={mockApplyUpdate}>
+      <Story />
+    </VersionProvider>
+  );
+}
