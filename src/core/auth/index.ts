034c0af5dce4bcadceb2d4cb88e2dfb73a389936
covik <6125218+Covik@users.noreply.github.com>
2023-07-03 01:07:25 +0200

    feat(app): add login-page in core/auth

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -3,3 +3,4 @@ export * from './ui/pages/session-loading-view';
 export * from './ui/atoms/session-loading-indicator';
 export * from './ui/molecules/session-error';
 export * from './ui/pages/session-error-view';
+export * from './login-page';
---
+++ src/core/auth/login-page.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { LoginView } from './ui/pages/login-view';
+import { Session } from '../../lib/SessionService';
+import type { LoginViewAttributes } from './ui/pages/login-view';
+
+export { testingSelectors } from './ui/pages/login-view';
+
+export interface LoginPageAttributes {
+  onSuccessfulAttempt: () => unknown;
+}
+
+export function LoginPage({ onSuccessfulAttempt }: LoginPageAttributes) {
+  const [currentState, goTo] =
+    useState<LoginViewAttributes['state']>('initial');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  async function tryLogin(email: string, password: string) {
+    goTo('submitting');
+
+    try {
+      await Session.create({ email, password });
+      Session.rememberForOneYear();
+      onSuccessfulAttempt();
+    } catch (e) {
+      if (e instanceof Session.ValidationException) {
+        goTo('validation-error');
+        !e.isEmailOk() && setEmailError('Email je obavezan');
+        !e.isPasswordOk() && setPasswordError('Lozinka je obavezna');
+      } else if (e instanceof Session.WrongCredentialsException) {
+        goTo('wrong-credentials');
+      } else {
+        goTo('unexpected-error');
+      }
+    }
+  }
+
+  return (
+    <LoginView
+      state={currentState}
+      emailError={emailError}
+      passwordError={passwordError}
+      onLoginAttempt={tryLogin}
+    />
+  );
+}
