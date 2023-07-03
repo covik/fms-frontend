308be979b6458a691214dcc03d926566738340e8
covik <6125218+Covik@users.noreply.github.com>
2023-07-03 02:14:53 +0200

    refactor(app): optimize imports of 'main'

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { QueryClient } from '@tanstack/query-core';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { AppVersionProvider } from '#core/version';
@@ -7,7 +6,7 @@ import { App } from './foundation';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <AppVersionProvider>
     <QueryClientProvider client={queryClient}>
       <App />
