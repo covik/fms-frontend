7d2ee5a3623b592998fc1a7af622d26146575d7c
covik <6125218+Covik@users.noreply.github.com>
2023-07-03 02:05:03 +0200

    refactor(app): make App component actually do something

--- src/foundation/App.tsx
+++ src/foundation/App.tsx
@@ -1,6 +1,18 @@
-import { SessionManager } from '#core/auth';
-import type { ReactNode } from 'react';
+import { AuthProvider, SessionManager } from '#core/auth';
+import { DateTimeProvider } from '#core/time';
+import { DesignBaseline } from './DesignBaseline';
+import { AuthenticatedApp } from './AuthenticatedApp';
 
-export function App({ children }: { children: ReactNode }) {
-  return <SessionManager>{children}</SessionManager>;
+export function App() {
+  return (
+    <DateTimeProvider>
+      <DesignBaseline>
+        <AuthProvider>
+          <SessionManager>
+            <AuthenticatedApp />
+          </SessionManager>
+        </AuthProvider>
+      </DesignBaseline>
+    </DateTimeProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { QueryClient } from '@tanstack/query-core';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { App, DesignBaseline } from './foundation';
-import { AuthenticatedApp } from './foundation/AuthenticatedApp';
-import { AuthProvider } from '#core/auth';
-import { DateTimeProvider } from '#core/time';
 import { AppVersionProvider } from '#core/version';
+import { App } from './foundation';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AppVersionProvider>
     <QueryClientProvider client={queryClient}>
-      <DateTimeProvider>
-        <DesignBaseline>
-          <AuthProvider>
-            <App>
-              <AuthenticatedApp />
-            </App>
-          </AuthProvider>
-        </DesignBaseline>
-      </DateTimeProvider>
+      <App />
     </QueryClientProvider>
   </AppVersionProvider>,
 );
