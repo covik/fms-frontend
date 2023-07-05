010b8874f89019ea34d959632644f4afcbad6242
covik <6125218+Covik@users.noreply.github.com>
2023-07-05 13:03:15 +0200

    feat(ui-organism): add vehicle-mileage-table

---
+++ src/features/vehicles/ui/organisms/vehicle-mileage-table/vehicle-mileage-table.stories.tsx
@@ -0,0 +1,55 @@
+import { VehicleMileageTable } from '.';
+import {
+  MileageTableRow,
+  MileageTableRowLoading,
+  MileageTableRowNoData,
+} from '../../molecules/mileage-table-row';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Organisms/Vehicle Mileage Table',
+  component: VehicleMileageTable,
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+    },
+  },
+} satisfies Meta<typeof VehicleMileageTable>;
+type Story = StoryObj<typeof VehicleMileageTable>;
+
+export const Default: Story = {
+  render: () => (
+    <VehicleMileageTable>
+      <MileageTableRow
+        name={'Test 1'}
+        mileage={'12000 km'}
+        barColor={'green'}
+        barWidthPercentage={100}
+      />
+      <MileageTableRow
+        name={'Test 2'}
+        mileage={'9000 km'}
+        barColor={'orange'}
+        barWidthPercentage={70}
+      />
+    </VehicleMileageTable>
+  ),
+};
+
+export const Loading: Story = {
+  render: () => (
+    <VehicleMileageTable>
+      <MileageTableRowLoading />
+      <MileageTableRowLoading />
+      <MileageTableRowLoading />
+    </VehicleMileageTable>
+  ),
+};
+
+export const NoData: Story = {
+  render: () => (
+    <VehicleMileageTable noHeader>
+      <MileageTableRowNoData />
+    </VehicleMileageTable>
+  ),
+};
---
+++ src/features/vehicles/ui/organisms/vehicle-mileage-table/vehicle-mileage-table.tsx
@@ -0,0 +1,45 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  tableCellClasses,
+} from '@mui/material';
+import { StyledRow } from '../../molecules/mileage-table-row';
+import type { ReactNode } from 'react';
+
+export interface VehicleMileageTableAttributes {
+  children: ReactNode;
+  noHeader?: boolean;
+}
+
+export function VehicleMileageTable({
+  children,
+  noHeader = false,
+}: VehicleMileageTableAttributes) {
+  return (
+    <TableContainer component={Paper}>
+      <Table
+        size={'small'}
+        sx={{
+          [`& .${tableCellClasses.root}`]: {
+            paddingX: 1,
+          },
+        }}
+      >
+        {noHeader ? null : (
+          <TableHead>
+            <StyledRow>
+              <TableCell align={'left'}>Vozilo</TableCell>
+              <TableCell align={'right'}>Kilometara</TableCell>
+              <TableCell></TableCell>
+            </StyledRow>
+          </TableHead>
+        )}
+        <TableBody>{children}</TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
