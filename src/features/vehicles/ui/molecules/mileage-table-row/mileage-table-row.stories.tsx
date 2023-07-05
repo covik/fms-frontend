70f841aa4053e948cbea0aed2b4fcef3ac7b6d50
covik <6125218+Covik@users.noreply.github.com>
2023-07-05 12:55:13 +0200

    feat(ui-molecule): add mileage-table-row

---
+++ src/features/vehicles/ui/molecules/mileage-table-row/mileage-table-row.stories.tsx
@@ -0,0 +1,48 @@
+import {
+  MileageTableRow,
+  MileageTableRowLoading,
+  MileageTableRowNoData,
+} from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Molecules/Mileage Table Row',
+  component: MileageTableRow,
+  decorators: [
+    (Story) => (
+      <table>
+        <tbody>
+          <Story />
+        </tbody>
+      </table>
+    ),
+  ],
+} satisfies Meta<typeof MileageTableRow>;
+type Story = StoryObj<typeof MileageTableRow>;
+
+export const Default: Story = {
+  args: {
+    name: 'Test',
+    mileage: '12000 km',
+    barWidthPercentage: 60,
+    barColor: 'green',
+  },
+};
+
+export const Loading: Story = {
+  render: () => <MileageTableRowLoading />,
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+    },
+  },
+};
+
+export const NoData: Story = {
+  render: () => <MileageTableRowNoData />,
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+    },
+  },
+};
---
+++ src/features/vehicles/ui/molecules/mileage-table-row/mileage-table-row.tsx
@@ -0,0 +1,79 @@
+import { Box, Skeleton, styled, TableCell, TableRow } from '@mui/material';
+import { CSSProperties } from 'react';
+
+const Bar = styled(Box)({
+  height: '5px',
+  borderRadius: '5px',
+});
+
+const StyledSkeleton = styled(Skeleton)({
+  margin: 'auto',
+});
+
+export const StyledRow = styled(TableRow)({
+  '&:last-child td, &:last-child th': { border: 0 },
+  '& > *:nth-of-type(1), & > *:nth-of-type(2)': {
+    width: '10%',
+    textAlign: 'center',
+  },
+  '& > *:nth-of-type(3)': {
+    width: '80%',
+  },
+});
+
+export interface MileageTableRowAttributes {
+  name: string;
+  mileage: string;
+  barColor: CSSProperties['color'];
+  barWidthPercentage: number;
+}
+
+export function MileageTableRow({
+  name,
+  mileage,
+  barColor,
+  barWidthPercentage,
+}: MileageTableRowAttributes) {
+  return (
+    <StyledRow>
+      <TableCell align={'left'}>{name}</TableCell>
+      <TableCell align={'right'} sx={{ width: '10%' }}>
+        {mileage}
+      </TableCell>
+      <TableCell sx={{ width: '80%' }}>
+        <Bar
+          sx={{
+            backgroundColor: barColor,
+            width: `${barWidthPercentage}%`,
+          }}
+        />
+      </TableCell>
+    </StyledRow>
+  );
+}
+
+export function MileageTableRowLoading() {
+  return (
+    <StyledRow>
+      <TableCell>
+        <StyledSkeleton variant={'text'} width={'45px'} />
+      </TableCell>
+      <TableCell>
+        <StyledSkeleton variant={'text'} width={'45px'} />
+      </TableCell>
+      <TableCell>
+        <StyledSkeleton variant={'text'} width={'100%'} />
+      </TableCell>
+    </StyledRow>
+  );
+}
+
+export function MileageTableRowNoData() {
+  return (
+    <StyledRow>
+      <TableCell align={'center'} width={'100%'} colSpan={3}>
+        Nema podataka
+      </TableCell>
+    </StyledRow>
+  );
+}
