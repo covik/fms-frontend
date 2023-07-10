d6fc6238dce686e6b36117f4d4a4b490f3f0e3a8
covik <6125218+Covik@users.noreply.github.com>
2023-07-10 17:15:04 +0200

    refactor(vehicles): simplify route-viewer and route queries

--- src/features/vehicles/queries/route.ts
+++ src/features/vehicles/queries/route.ts
@@ -1,10 +1,22 @@
+import { useMemo } from 'react';
 import { isToday } from 'date-fns';
 import { useQuery } from '@tanstack/react-query';
 import { VehicleService } from '../services/vehicle-service';
-import type { UseQueryResult } from '@tanstack/react-query';
+import type { Length, Speed } from '#lib/measurement-unit';
 import type { RoutePosition } from '../models/position';
 import type { RouteStop } from '../models/route-stop';
 import type { RouteSummary } from '../models/route-summary';
+
+export interface SummaryData {
+  totalDuration: number;
+  drivingDuration: number;
+  stopDuration: number;
+  distance: Length.BaseLength;
+  startOdometer: Length.BaseLength;
+  endOdometer: Length.BaseLength;
+  averageSpeed: Speed.BaseSpeed;
+  maxSpeed: Speed.BaseSpeed;
+}
 
 interface QueryParameters {
   vehicleId: string;
@@ -16,21 +28,22 @@ export function useRoutePositions({
   vehicleId,
   from,
   to,
-}: QueryParameters): UseQueryResult<RoutePosition[]> {
-  return useQuery({
+}: QueryParameters): RoutePosition[] | undefined {
+  const query = useQuery({
     queryKey: ['vehicles', vehicleId, 'routes', from, to],
     queryFn: ({ signal }) =>
       VehicleService.RouteService.fetchInRange({ vehicleId, from, to }, signal),
     staleTime: determineStaleTime(from, to),
   });
+  return query.data;
 }
 
 export function useRouteStops({
   vehicleId,
   from,
   to,
-}: QueryParameters): UseQueryResult<RouteStop[]> {
-  return useQuery({
+}: QueryParameters): RouteStop[] | undefined {
+  const query = useQuery({
     queryKey: ['vehicles', vehicleId, 'stops', from, to],
     queryFn: ({ signal }) =>
       VehicleService.RouteService.fetchStopsInRange(
@@ -39,14 +52,17 @@ export function useRouteStops({
       ),
     staleTime: determineStaleTime(from, to),
   });
+  return query.data;
 }
 
 export function useRouteSummary({
   vehicleId,
   from,
   to,
-}: QueryParameters): UseQueryResult<RouteSummary | NoSummary> {
-  return useQuery({
+}: QueryParameters): SummaryData | NoSummary | undefined {
+  const stops = useRouteStops({ vehicleId, from, to });
+
+  const { data: summary } = useQuery({
     queryKey: ['vehicles', vehicleId, 'summary', from, to],
     queryFn: async ({ signal }) => {
       try {
@@ -61,10 +77,43 @@ export function useRouteSummary({
     },
     staleTime: determineStaleTime(from, to),
   });
+
+  return useMemo(() => {
+    if (summary instanceof NoSummary) return summary;
+
+    if (summary === undefined || stops === undefined) return undefined;
+
+    return calculateFullSummary(summary, stops);
+  }, [summary, stops]);
 }
 
 export class NoSummary {}
 
 function determineStaleTime(from: Date, to: Date) {
   return isToday(from) || isToday(to) ? 60 * 1000 : Infinity;
+}
+
+function calculateFullSummary(
+  summary: RouteSummary,
+  stops: RouteStop[],
+): SummaryData {
+  const totalDuration = summary.durationInSeconds();
+  const { drivingDuration, stopDuration } =
+    summary.drivingAndStopDuration(stops);
+  const distance = summary.distance();
+  const startOdometer = summary.startOdometer();
+  const endOdometer = summary.endOdometer();
+  const averageSpeed = summary.averageSpeed();
+  const maxSpeed = summary.maxSpeed();
+
+  return {
+    totalDuration,
+    drivingDuration,
+    stopDuration,
+    distance,
+    startOdometer,
+    endOdometer,
+    averageSpeed,
+    maxSpeed,
+  };
 }
--- src/features/vehicles/route-history/ui/route-viewer/route-viewer.tsx
+++ src/features/vehicles/route-history/ui/route-viewer/route-viewer.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { Stack } from '@mui/material';
 import { Tile, TileNoContent, TileRawContent } from '#ui/molecules/tile';
 import {
@@ -16,9 +15,6 @@ import { RouteMap } from './route-map';
 import { RouteStopsTable } from './route-stops-table';
 import { RouteSummaryList } from './route-summary-list';
 import type { ReactNode } from 'react';
-import type { RouteSummary } from '../../../models/route-summary';
-import type { RouteStop } from '../../../models/route-stop';
-import type { SummaryData } from './route-summary-list';
 
 export interface RouteViewerAttributes {
   vehicleId: string;
@@ -33,21 +29,9 @@ export function RouteViewer({
   to,
   children = null,
 }: RouteViewerAttributes) {
-  const positionsQuery = useRoutePositions({ vehicleId, from, to });
-  const positions = useMemo(() => positionsQuery.data ?? [], [positionsQuery]);
-
-  const stopsQuery = useRouteStops({ vehicleId, from, to });
-  const stops = useMemo(() => stopsQuery.data ?? [], [stopsQuery]);
-
-  const summaryQuery = useRouteSummary({ vehicleId, from, to });
-  const summaryData: SummaryData | NoSummary | undefined = useMemo(() => {
-    if (summaryQuery.data instanceof NoSummary) return summaryQuery.data;
-
-    if (summaryQuery.data === undefined || stopsQuery.data === undefined)
-      return undefined;
-
-    return calculateFullSummary(summaryQuery.data, stops);
-  }, [summaryQuery.data, stopsQuery.data]);
+  const routePositions = useRoutePositions({ vehicleId, from, to });
+  const routeStops = useRouteStops({ vehicleId, from, to });
+  const routeSummary = useRouteSummary({ vehicleId, from, to });
 
   const spacing = 1;
   return (
@@ -56,51 +40,26 @@ export function RouteViewer({
         <Stack spacing={spacing}>
           {children}
           <Tile label={'Sažetak'}>
-            {summaryData instanceof NoSummary ? (
+            {routeSummary instanceof NoSummary ? (
               <TileNoContent>Nema informacija</TileNoContent>
             ) : (
-              <RouteSummaryList details={summaryData} />
+              <RouteSummaryList details={routeSummary} />
             )}
           </Tile>
           <Tile label={'Zaustavljanje'}>
-            {stopsQuery.data && stopsQuery.data.length === 0 ? (
+            {routeStops && routeStops.length === 0 ? (
               <TileNoContent>Nema zaustavljanja</TileNoContent>
             ) : (
               <TileRawContent>
-                <RouteStopsTable stops={stopsQuery.data} />
+                <RouteStopsTable stops={routeStops} />
               </TileRawContent>
             )}
           </Tile>
         </Stack>
       </GridSidebarTiles>
       <GridContent>
-        <RouteMap routes={positions} stops={stops} />
+        <RouteMap routes={routePositions ?? []} stops={routeStops ?? []} />
       </GridContent>
     </Grid>
   );
-}
-
-function calculateFullSummary(
-  summary: RouteSummary,
-  stops: RouteStop[],
-): SummaryData {
-  const totalDuration = summary.durationInSeconds();
-  const { drivingDuration, stopDuration } =
-    summary.drivingAndStopDuration(stops);
-  const distance = summary.distance();
-  const startOdometer = summary.startOdometer();
-  const endOdometer = summary.endOdometer();
-  const averageSpeed = summary.averageSpeed();
-  const maxSpeed = summary.maxSpeed();
-
-  return {
-    totalDuration,
-    drivingDuration,
-    stopDuration,
-    distance,
-    startOdometer,
-    endOdometer,
-    averageSpeed,
-    maxSpeed,
-  };
 }
