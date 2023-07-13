ebab396762e3aba52991dca23465a8efc5e0ddce
covik <6125218+Covik@users.noreply.github.com>
2023-07-13 14:32:43 +0200

    refactor(vehicles): use multiple-vehicles-tracking

--- src/features/vehicles/usecase/live-tracking/multiple-vehicles-tracking-page.tsx
+++ src/features/vehicles/usecase/live-tracking/multiple-vehicles-tracking-page.tsx
@@ -1,82 +1,53 @@
-import { useMemo } from 'react';
-import { CircularProgress, Paper } from '@mui/material';
+import { useMemo, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { AppMap, MapBounds } from '#core/map';
 import { VehicleService } from '../../services/vehicle-service';
-import { FixedPage, PagePadding } from '#ui/atoms/page';
-import { VehicleMapIcon } from '../../ui/components/vehicle-map-icon';
-import { VehicleMapMarker } from '../../ui/components/vehicle-map-marker';
-import type { ReactNode } from 'react';
+import { adaptLocatedVehicles } from '../../ui/adapters/vehicle-adapter';
+import { MultipleVehiclesTracking } from '../../ui/pages/multiple-vehicles-tracking';
+import type { Vehicle } from '../../ui/pages/multiple-vehicles-tracking';
+
+const today = new Date();
 
 export function MultipleVehiclesTrackingPage() {
   const query = useQuery({
     queryKey: ['vehicles'],
     queryFn: ({ signal }) => VehicleService.fetchAll(signal),
-    select: VehicleService.takeOnlyOperational,
+    select: (vehicles) => {
+      const operational = VehicleService.takeOnlyOperational(vehicles);
+      const unavailable = VehicleService.takeOnlyUnavailable(vehicles);
+
+      const sortedOperational = VehicleService.sortAscendingByName(operational);
+      const sortedUnavailable = VehicleService.sortAscendingByName(unavailable);
+      return [...sortedOperational, ...sortedUnavailable];
+    },
     refetchInterval: 2000,
   });
 
-  const operationalVehicles = query.data ?? [];
+  const rawVehicles = query.data;
 
-  const bounds = useMemo(
-    () =>
-      operationalVehicles.map((vehicle) => vehicle.position().coordinates()),
-    [operationalVehicles],
+  const adaptedVehicles = useMemo(
+    () => (rawVehicles ? adaptLocatedVehicles(rawVehicles) : rawVehicles),
+    [rawVehicles],
   );
 
+  const [selectedVehicleId, setSelectedVehicleId] = useState<
+    Vehicle['id'] | undefined
+  >(undefined);
+
+  function selectOrUnselectVehicle(vehicleId: Vehicle['id']) {
+    setSelectedVehicleId((currentSelection) =>
+      currentSelection === vehicleId ? undefined : vehicleId,
+    );
+  }
+
   return (
-    <FixedPage>
-      <PageContainer>
-        <AppMap sx={{ height: 'auto', width: '100%' }}>
-          {query.isFetching ? <FetchIndicator /> : null}
-          <MapBounds coordinates={bounds} once />
-          {operationalVehicles.map((vehicle) => (
-            <VehicleMapMarker
-              key={vehicle.id()}
-              position={vehicle.position().coordinates()}
-              name={vehicle.name()}
-            >
-              <VehicleMapIcon
-                ignitionOn={vehicle.hasIgnitionTurnedOn()}
-                moving={vehicle.isInMotion()}
-                angleInDegrees={vehicle.course().value()}
-              />
-            </VehicleMapMarker>
-          ))}
-        </AppMap>
-      </PageContainer>
-    </FixedPage>
+    <MultipleVehiclesTracking
+      vehicles={adaptedVehicles}
+      routeDate={today}
+      selectedVehicleId={selectedVehicleId ?? ''}
+      onSelectionChange={selectOrUnselectVehicle}
+      dataRefreshInProgress={query.isRefetching}
+    />
   );
 }
 
 export default MultipleVehiclesTrackingPage;
-
-function PageContainer({ children }: { children: ReactNode | ReactNode[] }) {
-  return (
-    <PagePadding flex={1} display={'flex'}>
-      {children}
-    </PagePadding>
-  );
-}
-
-function FetchIndicator() {
-  return (
-    <Paper
-      elevation={0}
-      sx={{
-        padding: 1,
-        position: 'absolute',
-        zIndex: 1,
-        bottom: '10px',
-        left: '10px',
-      }}
-    >
-      <CircularProgress
-        sx={{ display: 'block' }}
-        disableShrink={true}
-        thickness={5}
-        size={20}
-      />
-    </Paper>
-  );
-}
