c696ccdac7c1cead2b6dac2b4901b225c6257e44
covik <6125218+Covik@users.noreply.github.com>
2023-07-14 11:50:59 +0200

    refactor(vehicles,fixture): simplify type of position converters

--- src/features/vehicles/fixtures/view/route-positions.ts
+++ src/features/vehicles/fixtures/view/route-positions.ts
@@ -2,12 +2,9 @@ import { format } from 'date-fns';
 import { Length, Speed, Voltage } from '#lib/measurement-unit';
 import positions from '../model/route-positions';
 import { adaptRoutePositions } from '../../ui/adapters/route-position';
-import type { UnitFormatters } from '../../queries';
+import type { PositionFormatters } from '../../ui/adapters/route-position';
 
-const converters: Pick<
-  UnitFormatters,
-  'formatDateTime' | 'formatLength' | 'formatSpeed' | 'formatVoltage'
-> = {
+const converters: PositionFormatters = {
   formatDateTime: (unit) => format(unit, 'dd.MM.yyyy. HH:mm:ss'),
   formatLength: (unit) => Length.adaptiveFormat(unit, 1),
   formatSpeed: (unit) => Speed.format(Speed.convert(unit).toKph()),
