5a9676273315e034647979878912c19e8ae56f50
covik <6125218+Covik@users.noreply.github.com>
2023-07-14 14:03:13 +0200

    refactor(map): remove comment

--- src/core/map/map-fetch-indicator.tsx
+++ src/core/map/map-fetch-indicator.tsx
@@ -2,11 +2,6 @@ import { CircularProgress, Paper, paperClasses } from '@mui/material';
 
 export const className = paperClasses.root;
 
-/* position: 'absolute',
-        zIndex: 1,
-        bottom: '10px',
-        left: '10px', */
-
 export function MapFetchIndicator() {
   return (
     <Paper elevation={0} sx={{ padding: 1, width: 'fit-content' }}>
