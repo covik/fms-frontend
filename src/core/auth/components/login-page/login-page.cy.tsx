d903273e328ae1a900a1c0e9f71966f124de4c0b
covik <6125218+Covik@users.noreply.github.com>
2023-08-20 10:42:06 +0200

    test(auth): remove extra describe block

--- src/core/auth/components/login-page/login-page.cy.tsx
+++ src/core/auth/components/login-page/login-page.cy.tsx
@@ -4,104 +4,102 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { LoginPage, testingSelectors } from '.';
 import { SessionService } from '../../services';
 
-describe(LoginPage.name, () => {
-  beforeEach(() => {
-    cy.mount(
-      <QueryClientProvider client={createTestClient()}>
-        <LoginPage />
-      </QueryClientProvider>,
-    );
-  });
+beforeEach(() => {
+  cy.mount(
+    <QueryClientProvider client={createTestClient()}>
+      <LoginPage />
+    </QueryClientProvider>,
+  );
+});
 
-  it('should render in initial state', () => {
-    cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`)
-      .should('be.visible')
-      .and('not.be.disabled');
+it('should render in initial state', () => {
+  cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`)
+    .should('be.visible')
+    .and('not.be.disabled');
 
-    cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`)
-      .should('be.visible')
-      .and('not.be.disabled');
+  cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`)
+    .should('be.visible')
+    .and('not.be.disabled');
 
-    cy.get(`[data-testid="${testingSelectors.inputs.submit}"]`)
-      .should('be.visible')
-      .and('not.be.disabled');
-  });
+  cy.get(`[data-testid="${testingSelectors.inputs.submit}"]`)
+    .should('be.visible')
+    .and('not.be.disabled');
+});
 
-  it('should go to submitting state after clicking a submit button', () => {
-    simulateSubmittingState();
+it('should go to submitting state after clicking a submit button', () => {
+  simulateSubmittingState();
 
-    submitForm();
+  submitForm();
 
-    cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`)
-      .should('be.visible')
-      .and('be.disabled');
+  cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`)
+    .should('be.visible')
+    .and('be.disabled');
 
-    cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`)
-      .should('be.visible')
-      .and('be.disabled');
+  cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`)
+    .should('be.visible')
+    .and('be.disabled');
 
-    cy.get(`[data-testid="${testingSelectors.inputs.submit}"]`)
-      .should('be.visible')
-      .and('be.disabled');
-  });
+  cy.get(`[data-testid="${testingSelectors.inputs.submit}"]`)
+    .should('be.visible')
+    .and('be.disabled');
+});
 
-  it('should go to validation error state and show error message if email is empty', () => {
-    cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`).type(
-      'strong-password',
-    );
+it('should go to validation error state and show error message if email is empty', () => {
+  cy.get(`[data-testid="${testingSelectors.inputs.password}"] input`).type(
+    'strong-password',
+  );
 
-    submitForm();
+  submitForm();
 
-    cy.contains('Email je obavezan').should('be.visible');
-    cy.contains('Lozinka je obavezna').should('not.exist');
-  });
+  cy.contains('Email je obavezan').should('be.visible');
+  cy.contains('Lozinka je obavezna').should('not.exist');
+});
 
-  it('should go to validation error state and show error message if password is empty', () => {
-    cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`).type(
-      'me@example.com',
-    );
+it('should go to validation error state and show error message if password is empty', () => {
+  cy.get(`[data-testid="${testingSelectors.inputs.email}"] input`).type(
+    'me@example.com',
+  );
 
-    submitForm();
+  submitForm();
 
-    cy.contains('Lozinka je obavezna').should('be.visible');
-    cy.contains('Email je obavezan').should('not.exist');
-  });
+  cy.contains('Lozinka je obavezna').should('be.visible');
+  cy.contains('Email je obavezan').should('not.exist');
+});
 
-  it('should go to error state and show error message if credentials do not match', () => {
-    simulateWrongCredentialsSituation();
-    fillForm();
+it('should go to error state and show error message if credentials do not match', () => {
+  simulateWrongCredentialsSituation();
+  fillForm();
 
-    cy.contains('Pogrešan email ili lozinka').should('not.exist');
-    submitForm();
-    cy.contains('Pogrešan email ili lozinka').should('be.visible');
-  });
+  cy.contains('Pogrešan email ili lozinka').should('not.exist');
+  submitForm();
+  cy.contains('Pogrešan email ili lozinka').should('be.visible');
+});
 
-  it('should go to error state and show error message if server error occurs', () => {
-    simulateServerError();
-    fillForm();
+it('should go to error state and show error message if server error occurs', () => {
+  simulateServerError();
+  fillForm();
 
-    cy.contains('Došlo je do neočekivane greške').should('not.exist');
-    submitForm();
-    cy.contains('Došlo je do neočekivane greške').should('be.visible');
-  });
+  cy.contains('Došlo je do neočekivane greške').should('not.exist');
+  submitForm();
+  cy.contains('Došlo je do neočekivane greške').should('be.visible');
+});
 
-  it('should remember session for one year after successful login', () => {
-    const now = new Date();
-    const oneYearFromNow = addDays(now, 365);
-    cy.clock(now);
+it('should remember session for one year after successful login', () => {
+  const now = new Date();
+  const oneYearFromNow = addDays(now, 365);
+  cy.clock(now);
 
-    simulateCorrectCredentialsSituation();
-    fillForm();
+  simulateCorrectCredentialsSituation();
+  fillForm();
 
-    const [session, id] = simulateSessionCookieSetByBackend();
-    submitForm();
+  const [session, id] = simulateSessionCookieSetByBackend();
+  submitForm();
 
-    cy.getCookie(session).should('deep.include', {
-      name: session,
-      value: id,
-      expiry: getUnixTime(oneYearFromNow),
-      path: '/',
-    });
+  cy.getCookie(session).should('deep.include', {
+    name: session,
+    value: id,
+    expiry: getUnixTime(oneYearFromNow),
+    path: '/',
   });
 });
 
