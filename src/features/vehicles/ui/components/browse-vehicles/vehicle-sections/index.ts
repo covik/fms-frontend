5bb1f183795a6ad3666392be7cceb4a2fbcf3433
covik <6125218+Covik@users.noreply.github.com>
2023-08-22 18:43:46 +0200

    fix: wip

--- src/features/vehicles/ui/components/browse-vehicles/vehicle-sections/index.ts
+++ src/features/vehicles/ui/components/browse-vehicles/vehicle-sections/index.ts
@@ -1,3 +1,4 @@
 export { VehicleSections } from './section';
+export * from './loading-section';
 export * from './section-operational-vehicles';
 export * from './section-unavailable-vehicles';
--- src/features/vehicles/ui/components/browse-vehicles/vehicles-loading.tsx
+++ src/features/vehicles/ui/components/browse-vehicles/vehicles-loading.tsx
@@ -1,27 +1,13 @@
 import { VehicleListItemLoading } from './vehicle-list-item';
-import {
-  Section,
-  SectionContent,
-  SectionHeader,
-  SectionTitle,
-} from './vehicle-sections/section';
-import { Skeleton } from '@mui/material';
+import { LoadingSection } from './vehicle-sections';
 
 export function VehiclesLoading() {
   return (
-    <Section>
-      <SectionHeader>
-        <SectionTitle>
-          <Skeleton variant={'text'} width={'60px'} />
-        </SectionTitle>
-      </SectionHeader>
-
-      <SectionContent>
-        <VehicleListItemLoading />
-        <VehicleListItemLoading />
-        <VehicleListItemLoading />
-        <VehicleListItemLoading />
-      </SectionContent>
-    </Section>
+    <LoadingSection>
+      <VehicleListItemLoading />
+      <VehicleListItemLoading />
+      <VehicleListItemLoading />
+      <VehicleListItemLoading />
+    </LoadingSection>
   );
 }
