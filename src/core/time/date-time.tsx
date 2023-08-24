551a2d2fd8cec888e0d126fb08ded28e93b09e5b
covik <6125218+Covik@users.noreply.github.com>
2023-08-24 17:02:00 +0200

    fix: wip

--- src/core/time/date-time.tsx
+++ src/core/time/date-time.tsx
@@ -2,11 +2,8 @@ import { createContext, useCallback, useContext, useMemo } from 'react';
 import { hr } from 'date-fns/locale';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import {
-  format,
-  formatDistanceToNowStrict,
-  intervalToDuration,
-} from 'date-fns';
+import { format, formatDistanceToNowStrict } from 'date-fns';
+import { formatDuration } from './format-duration';
 import type {
   DateTimeAPI,
   DateTimeOptions,
@@ -79,20 +76,4 @@ export function useDateTime(): DateTimeAPI {
     formatDuration,
     formatDateTime,
   };
-}
-
-function formatDuration(durationInSeconds: number) {
-  if (durationInSeconds < 60) return `${durationInSeconds}s`;
-
-  const duration = intervalToDuration({
-    start: 0,
-    end: durationInSeconds * 1000,
-  });
-  const [hours, minutes] = [duration.hours ?? 0, duration.minutes ?? 0];
-
-  const valuesWithSymbol = [];
-  if (hours > 0) valuesWithSymbol.push(`${hours}h`);
-  if (minutes > 0) valuesWithSymbol.push(`${minutes}m`);
-
-  return valuesWithSymbol.join(' ');
 }
