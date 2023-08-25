509ac5fe23051a0cffb6a64f2d8de4129a826290
covik <6125218+Covik@users.noreply.github.com>
2023-08-25 12:00:03 +0200

    feat(ui): add app-bar example

--- src/ui/app-bar/app-bar.stories.tsx
+++ src/ui/app-bar/app-bar.stories.tsx
@@ -1,4 +1,8 @@
-import { AppBar } from '.';
+import { AppBar, AreaControlCenter, AreaContent, AreaMenu } from '.';
+import {
+  Menu as MenuIcon,
+  ChevronUp as ControlCenterIcon,
+} from 'mdi-material-ui';
 import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
@@ -7,4 +11,16 @@ export default {
 } satisfies Meta<typeof AppBar>;
 type Story = StoryObj<typeof AppBar>;
 
-export const Transparent: Story = {};
+export const Transparent: Story = {
+  render: () => (
+    <AppBar>
+      <AreaMenu>
+        <MenuIcon />
+      </AreaMenu>
+      <AreaContent>App Bar</AreaContent>
+      <AreaControlCenter>
+        <ControlCenterIcon />
+      </AreaControlCenter>
+    </AppBar>
+  ),
+};
--- src/ui/app-bar/app-bar.tsx
+++ src/ui/app-bar/app-bar.tsx
@@ -1,9 +1,32 @@
-export function AppBar() {
-  return null;
+import { styled } from '@mui/material';
+import type { ReactNode } from 'react';
+
+const name = 'AppBar';
+
+const AppBarRoot = styled('header', {
+  name,
+  slot: 'Root',
+})({});
+
+export interface AppBarAttributes {
+  children: ReactNode;
 }
 
-export function MenuButton() {}
+export function AppBar({ children }: AppBarAttributes) {
+  return <AppBarRoot>{children}</AppBarRoot>;
+}
 
-export function ControlCenterButton() {}
+export const AreaContent = styled('section', {
+  name,
+  slot: 'Content',
+})({});
 
-export function Main() {}
+export const AreaMenu = styled('section', {
+  name,
+  slot: 'Menu',
+})({});
+
+export const AreaControlCenter = styled('section', {
+  name,
+  slot: 'ControlCenter',
+})({});
